5ce518db87e363a9b07de6569bfad491fccd8787
Patrick Louis Aldover <pl.aldover@gmail.com>
2024-03-05 23:53:44 +0100

    Fixed missing shots, adjusted court dimensions

--- src/js/main.js
+++ src/js/main.js
@@ -122,28 +122,25 @@ function update_players(url, player_selected, select_name) {
             positions = []
             for (shot of shots) {
                 if (shot.PLAYER_NAME == player_selected) {
+                    sec = shot.SECS_LEFT
+                    min = shot.MINS_LEFT
                     quarter = shot.QUARTER
-                    min = (quarter <= 4) ? (12 - shot.MINS_LEFT) : (5 - shot.MINS_LEFT) 
-                    sec = 60 - shot.SECS_LEFT
                     time = min + ':' + sec + ' (' + ((quarter <= 4) ? (quarter + '. Quarter') : ((quarter - 4) + '. Overtime')) + ')'
-                    positions.push({'x': shot.LOC_X, 
-                                    'y': shot.LOC_Y, 
+                    positions.push({'x': parseFloat(shot.LOC_X), 
+                                    'y': parseFloat(shot.LOC_Y), 
                                     'shot_made': shot.SHOT_MADE == 'True',
                                     'distance': shot.SHOT_DISTANCE + ' ft',
                                     'time': time
                                     })
                 }
             }
-
+            
+            // shot locations for years 2020-2022 are flawed (newly computed in shotchart.js)
             years = ['2022', '2021', '2020']
-            adjust_position = false
-            for (year of years) {
-                if (url.indexOf(year) !== -1) {
-                    adjust_position = true
-                    break
-                }
-            }
-            draw_shot_chart(positions, adjust_position)
+            regex = /\/(\d{4})\//
+            year = regex.exec(url)[1]
+            
+            draw_shot_chart(positions, years.includes(year))
         },
         dataType: "text",
         complete: function () {
@@ -154,23 +151,25 @@ function update_players(url, player_selected, select_name) {
 
 init_page()
 
+// TODO: disable before deployment
 /*
 function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
 async function debug_selector() {
-    $("#season_selector").val('2023')
-    $("#date_selector").val('04-04-2023')
-    update_games('2023', null)
+    $("#season_selector").val('2021')
+    update_dates('2021', null)
     await sleep(100)
-    $("#game_selector").val('Philadelphia76ers_BostonCeltics')
+    $("#date_selector").val('01-03-2021')
+    update_games('2021', null)
+    await sleep(100)
+    $("#game_selector").val('PortlandTrailBlazers_GoldenStateWarriors')
     update_page(false)
-    $("#player_selector").val('Joel Embiid')
+    await sleep(100)
+    $("#player_selector").val('Stephen Curry')
     update_page(false)
 }
 
-
-//debug_selector()
 setTimeout(debug_selector, 100)
 */
--- src/js/shotchart.js
+++ src/js/shotchart.js
@@ -1,5 +1,5 @@
 
-const width = 600
+const width = 800
 const height = 500
 const scale = 10
 const center_x = width / 2
@@ -10,104 +10,105 @@ const line_thickness = 3
 
 function draw_shot_chart(positions, adjust_position) {
 
-    $('#container').empty()
-    const svg = d3.select('#container')
+    $('#svg_shots').empty()
+    $('#svg_court').empty()
+    const svg_shots = d3.select('#svg_shots')
                     .append('svg')
                     .attr('width', width)
                     .attr('height', height)
+    const svg_court = d3.select('#svg_court')
+                    .append('svg')
+                    .attr('width', width)
+                    .attr('height', height)
+
+    // basket
+    const width_board = 80
+    const y_board = center_y - 40
+    draw_line(svg_court,    { x: center_x - width_board / 2, y: y_board }, 
+                            { x: center_x + width_board / 2, y: y_board })
+    draw_circle(svg_court, center_x, y_board - 15, 12)
+
+    const width_zone = 160
+    const height_zone = 190
+    const height_baseline = y_board + 40
+    draw_rect(svg_court, center_x - width_zone / 2, height_baseline - height_zone, width_zone, height_zone)
+    
+    draw_arc(svg_court, center_x, center_y - 42, radius, -Math.PI / 2, Math.PI / 2, false)
+
+    draw_arc(svg_court, center_x, height_baseline - height_zone, width_zone / 3, -Math.PI / 2, Math.PI / 2, false)
+    draw_arc(svg_court, center_x, height_baseline - height_zone, width_zone / 3, Math.PI / 2,  3 * Math.PI / 2, true)
+
+    const width_baseline = 500
+    draw_line(svg_court,    { x: center_x - width_baseline / 2, y: height_baseline }, 
+                            { x: center_x + width_baseline / 2, y: height_baseline })
+    
+    const width_3pt = 430
+    const height_3pt = 150
+    draw_line(svg_court,    { x: center_x - width_3pt / 2, y: height_baseline }, 
+                            { x: center_x - width_3pt / 2, y: height_baseline - height_3pt })
+    draw_line(svg_court,    { x: center_x + width_3pt / 2, y: height_baseline }, 
+                            { x: center_x + width_3pt / 2, y: height_baseline - height_3pt })
+    draw_arc(svg_court, center_x, height_baseline - 50,  2.75 * width_3pt / 5, -Math.PI / 2.75, Math.PI / 2.75, false)
+
     // background
-    svg.append('rect')
-        .attr('width', width)
+    svg_court.append('rect')
+        .attr('x', center_x - width_baseline / 2)
+        .attr('y', height_baseline - height)
+        .attr('width', width_baseline)
         .attr('height', height)
         .attr('fill', '#6f6fff')
         .lower()
-    
-    // basket
-    const width_board = 80
-    const y_board = center_y - 25
-    draw_line(svg,  { x: center_x - width_board / 2, y: y_board }, 
-                    { x: center_x + width_board / 2, y: y_board })
-    draw_line(svg,  { x: center_x, y: y_board }, 
-                    { x: center_x, y: y_board - 15 })
-    draw_circle(svg, center_x, y_board - 27, 12)
 
-    const width_zone = 160
-    const height_zone = 205
-    const height_baseline = y_board + 40
-    draw_rect(svg, center_x - width_zone / 2, height_baseline - height_zone, width_zone, height_zone)
-    
-    draw_arc(svg, center_x, center_y - 42, radius, -Math.PI / 2, Math.PI / 2, false)
-
-    draw_arc(svg, center_x, height_baseline - height_zone, width_zone / 3, -Math.PI / 2, Math.PI / 2, false)
-    draw_arc(svg, center_x, height_baseline - height_zone, width_zone / 3, Math.PI / 2,  3 * Math.PI / 2, true)
-
-    const width_baseline = 600
-    draw_line(svg,  { x: center_x - width_baseline / 2, y: height_baseline }, 
-                    { x: center_x + width_baseline / 2, y: height_baseline })
-    
-    const width_3pt = 500
-    const height_3pt = 150
-    draw_line(svg,  { x: center_x - width_3pt / 2, y: height_baseline }, 
-                    { x: center_x - width_3pt / 2, y: height_baseline - height_3pt })
-    draw_line(svg,  { x: center_x + width_3pt / 2, y: height_baseline }, 
-                    { x: center_x + width_3pt / 2, y: height_baseline - height_3pt })
-    draw_arc(svg, center_x, height_baseline - 5,  3 * width_3pt / 5 - 11.5, -Math.PI / 3, Math.PI / 3, false)
-    
-    /*
-    TODO: The positions from 2020-2022 are incorrect (or at least falsely scaled)
-    => requires manual scaling
-
-    use high-point games as reference
-    e.g. 
-    2020: Lillard (61) vs. Golden State Warriors
-    2021: Curry (62) vs Portland Trailblazers
-    2022: Irving (60) vs Orlando Magic
-    */
+    // shot locations for years 2020-2022 are flawed (newly computed in shotchart.js)
     x_adjustment = 1
     y_adjustment = 1
     if (adjust_position) {
-        x_adjustment = 11
-        y_adjustment = 8
+        x_adjustment = 10
+        y_adjustment = 10
     }
 
     // shots
-    svg.selectAll('circle')
-        .data(positions)
-        .enter()
-        .append('circle')
-        .attr('cx', function(d) { 
-            const cx = parseFloat(center_x) + parseFloat(d.x) * scale * x_adjustment
-            return cx
-        })
-        .attr('cy', function(d) { 
-            const correction_y = (y_adjustment > 1) ? (y_adjustment - 1) * 55 : 0 
-            const cy = parseFloat(center_y) - parseFloat(d.y) * scale * y_adjustment + correction_y
-            return cy
-        })
-        .attr('r', 0)
-        .attr('opacity', 0)
-        .transition()
-        .delay(function(d, i) { return i * 10 })
-        .duration(500)
-        .attr('r', 5)
-        .attr('opacity', 1)
-        .style('fill', function(d) { return d.shot_made ? '#35f086' : '#f03535' })
-        .selection()
-        .on('mouseenter', function(event, d) {
-            const x_label = parseFloat(center_x) + parseFloat(d.x) * scale
-            const y_label = parseFloat(center_y) - parseFloat(d.y) * scale
-            d3.select(this.parentNode)
-              .append('text')
-              .text(d.time)
-              .attr('class', 'label')
-              .attr('x', x_label + offset_label) 
-              .attr('y', y_label - offset_label) 
-              .style('display', 'block') 
-        })
-        .on('mouseleave', function(event, d) { d3.select('.label').remove() })
+    svg_shots.selectAll('circle')
+            .data(positions)
+            .enter()
+            .append('circle')
+            .attr('cx', function(d) { 
+                const cx = center_x + d.x * scale * x_adjustment
+                return cx
+            })
+            .attr('cy', function(d) { 
+                const correction_y = (y_adjustment > 1) ? (y_adjustment - 1) * 59 : 0 // for flawed shot locations (2020-2022)
+                const cy = center_y - d.y * scale * y_adjustment + correction_y
+                return cy
+            })
+            .attr('r', 0)
+            .attr('opacity', 0)
+            .transition()
+            .delay(function(d, i) { return i * 10 })
+            .duration(500)
+            .attr('r', 5)
+            .attr('opacity', 1)
+            .style('fill', function(d) { return d.shot_made ? '#35f086' : '#f03535' })
+            .selection()
+            .on('mouseenter', function(event, d) {
+                const x_label = center_x + d.x * scale * x_adjustment
+                const correction_y = (y_adjustment > 1) ? (y_adjustment - 1) * 59 : 0 // for flawed shot locations (2020-2022)
+                const y_label = center_y - d.y * scale * y_adjustment + correction_y
+                d3.select(this.parentNode)
+                .append('text')
+                .text(d.time)
+                .attr('class', 'label')
+                .attr('x', x_label + offset_label) 
+                .attr('y', y_label - offset_label) 
+                .style('display', 'block') 
+            })
+            .on('mouseleave', function(event, d) { d3.select('.label').remove() })
+                    
     
-    const svg_scale = 1
-    svg.attr('transform',  `scale(${svg_scale})`)
+    const svg_scale = 1.5
+    svg_shots.attr('transform',  `scale(${svg_scale})`)
+    svg_court.attr('transform',  `scale(${svg_scale})`)
+
 }
 
 function draw_line(svg, p1, p2) {
@@ -137,13 +138,17 @@ function draw_rect(svg, x, y, width, height) {
 }
 
 function draw_circle(svg, x, y, r) {
-    svg.append('circle')
-        .attr('cx', x)
-        .attr('cy', y)
+    svg.selectAll('circles')
+        .data([{ x: x, y: y }])
+        .enter()
+        .append('circle')
+        .attr('cx', function (d) { return d.x })
+        .attr('cy', function (d) { return d.y })
         .attr('r', r)
         .attr('stroke', 'black')
         .attr('stroke-width', line_thickness)
         .attr('fill', 'none')
+
 }
 
 function draw_arc(svg, x, y, r, start, end, stroke) {
